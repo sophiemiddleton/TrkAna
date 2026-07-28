3c8ca19c6da021cb32dc7790c042751deaba2627
sophieMu2e <sophie@fnal.gov>
2026-07-28 10:06:15 -0500

    updated to use simparts

--- src/RNTuplemaker_module.cc
+++ src/RNTuplemaker_module.cc
@@ -9,65 +9,70 @@
 #include "art/Framework/Principal/Handle.h"
 
 #include "TFile.h"
-#include "ROOT/RNTuple.hxx"
-#include "ROOT/RNTupleWriter.hxx"
-#include "ROOT/RField.hxx"
-#include "ROOT/RNTupleModel.hxx"
+#include "canvas/Utilities/InputTag.h"
 
-#include "TError.h" // Needed for gErrorIgnoreLevel and kError
+// ROOT & RNTuple headers
+#include "TError.h"
+#include "ROOT/RNTuple.hxx"
+#include "ROOT/RNTupleModel.hxx"
+#include "ROOT/RNTupleWriter.hxx"
+
+// Mu2e Data Product Headers
+#include "Offline/MCDataProducts/inc/SimParticle.hh"
+
 namespace mu2e {
+
 class RNTuplemaker : public art::EDAnalyzer {
 public:
   struct Config {
-        using Name=fhicl::Name;
-        using Comment=fhicl::Comment;
-        fhicl::Atom<int> diagLevel{Name("diagLevel"), Comment("diagLevel")};
-        };
+    using Name = fhicl::Name;
+    using Comment = fhicl::Comment;
+
+    fhicl::Atom<int> diagLevel{Name("diagLevel"), Comment("Diagnostic level"), 0};
+    fhicl::Atom<art::InputTag> simsTag{
+        Name("simsTag"), 
+        Comment("InputTag for SimParticleCollection"), 
+        art::InputTag("compressRecoMCs")};
+  };
 
   typedef art::EDAnalyzer::Table<Config> Parameters;
 
-      explicit RNTuplemaker(const Parameters& conf);
-      virtual ~RNTuplemaker() { }
+  explicit RNTuplemaker(const Parameters& conf);
+  virtual ~RNTuplemaker() {}
 
-      void beginJob() override;
-      //void beginSubRun(const art::SubRun & subrun ) override;
-      void analyze(const art::Event& e) override;
-      void endJob() override;
-      
+  void beginJob() override;
+  void analyze(const art::Event& e) override;
+  void endJob() override;
 
 private:
   Config _conf;
-  int _diagLevel;
+  art::InputTag _simsTag;
+
   std::unique_ptr<ROOT::Experimental::RNTupleWriter> writer_;
-  std::shared_ptr<float> fldEnergy_;
+
+  // Scalar Fields bound to RNTuple Model (assumes single entry per event)
+  std::shared_ptr<int>   fldHasElec_;
+  std::shared_ptr<int>   fldElecPdg_;
+  std::shared_ptr<float> fldElecMom_;
+  std::shared_ptr<float> fldElecTime_;
 };
 
-RNTuplemaker::RNTuplemaker(const Parameters& conf):
-    art::EDAnalyzer(conf),
-    _conf(conf()),
-    _diagLevel(conf().diagLevel())
-{
+RNTuplemaker::RNTuplemaker(const Parameters& conf)
+    : art::EDAnalyzer(conf),
+      _conf(conf()),
+      _simsTag(conf().simsTag()) {
   gErrorIgnoreLevel = kError;
   SetErrorHandler(DefaultErrorHandler);
 }
 
-/*void RNTuplemaker::beginJob() {
-  auto model = ROOT::Experimental::RNTupleModel::Create();
-  fldEnergy_ = model->MakeField<float>("energy");
-  
-  writer_ = ROOT::Experimental::RNTupleWriter::Recreate(
-      std::move(model), 
-      "myNtuple",                        // Ntuple name
-      "nts.owner.description.version.root" // Output file name
-  );
-}*/
-
-
 void RNTuplemaker::beginJob() {
-  art::ServiceHandle<art::TFileService> tfs;
-
   auto model = ROOT::Experimental::RNTupleModel::Create();
-  fldEnergy_ = model->MakeField<float>("energy");
+   art::ServiceHandle<art::TFileService> tfs;
+
+  fldHasElec_  = model->MakeField<int>("hasElec");
+  fldElecPdg_   = model->MakeField<int>("elec_pdg");
+  fldElecMom_   = model->MakeField<float>("elec_mom");
+  fldElecTime_  = model->MakeField<float>("elec_time");
 
   std::string filename = tfs->file().GetName();
 
@@ -80,17 +85,42 @@ void RNTuplemaker::beginJob() {
   );
 }
 
-void RNTuplemaker::analyze(const art::Event& e){
-  // Fill the energy field with dummy data
-  *fldEnergy_ = 105.0f;
-  
-  // Write the entry to the ntuple
+void RNTuplemaker::analyze(const art::Event& e) {
+
+  *fldHasElec_  = 0;
+  *fldElecPdg_   = 0;
+  *fldElecMom_   = 0.0f;
+  *fldElecTime_  = 0.0f;
+
+  auto simsHandle = e.getValidHandle<SimParticleCollection>(_simsTag);
+
+  const mu2e::SimParticle* signalElectron = nullptr;
+
+  for (const auto& kv : *simsHandle) {
+    const auto& particle = kv.second;
+
+    if (particle.pdgId() == 11 && particle.isPrimary()) {
+      signalElectron = &particle;
+      break;
+    }
+  }
+
+  // Populate scalar fields if electron is present (FIXME this is still a dummy)
+  if (signalElectron != nullptr) {
+    *fldHasElec_  = 1;
+    *fldElecPdg_   = signalElectron->pdgId();
+    *fldElecMom_   = static_cast<float>(signalElectron->startMomentum().vect().mag());
+    *fldElecTime_  = static_cast<float>(signalElectron->startGlobalTime());
+  }
+
+  // Write ONE row per event
   writer_->Fill();
 }
 
 void RNTuplemaker::endJob() {
-  // Explicitly destroy the writer to flush buffers & write the RNTuple footer
-  writer_.reset(); 
+  writer_.reset();
 }
-}
+
+} // namespace mu2e
+
 DEFINE_ART_MODULE(mu2e::RNTuplemaker)
