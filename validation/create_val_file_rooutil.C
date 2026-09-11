7f48e8191e80d91094bc9e080d0b94d946d26627
sophieMu2e <sophie@fnal.gov>
2026-09-11 11:28:33 -0500

    moved constants

--- validation/create_val_file_rooutil.C
+++ validation/create_val_file_rooutil.C
@@ -491,6 +491,11 @@ void create_val_file_rooutil(std::string filename, std::string outfilename) {
   TH1F* h_caloclusters_hits_ = new TH1F("h_caloclusters_hits_", "", 100,0,100);
   TH1F* h_caloclusters_size_ = new TH1F("h_caloclusters_size_", "", 100,0,100);
   TH1F* h_caloclusters_isSplit_ = new TH1F("h_caloclusters_isSplit_", "", 2,0,2);
+  TH1F* h_caloclusters_secondMoment = new TH1F("h_caloclusters_secondMoment", "", 100,0,500);
+  TH1F* h_caloclusters_e1 = new TH1F("h_caloclusters_e1", "", 100,0,100);
+  TH1F* h_caloclusters_e2 = new TH1F("h_caloclusters_e2", "", 100,0,100);
+  TH1F* h_caloclusters_e9 = new TH1F("h_caloclusters_e9", "", 100,0,100);
+  TH1F* h_caloclusters_e25 = new TH1F("h_caloclusters_e25", "", 100,0,100);
 
   TH1F* h_calohits_crystalId_ = new TH1F("h_calohits_crystalId_", "", 2,0,2);
   TH1F* h_calohits_nSiPMs_ = new TH1F("h_calohits_nSiPMs_", "", 100,0,100);
@@ -1185,6 +1190,11 @@ void create_val_file_rooutil(std::string filename, std::string outfilename) {
         }
         h_caloclusters_size_->Fill(calocluster.size_);
         h_caloclusters_isSplit_->Fill(calocluster.isSplit_);
+        h_caloclusters_secondMoment->Fill(calocluster.secondMoment);
+        h_caloclusters_e1->Fill(calocluster.e1);
+        h_caloclusters_e2->Fill(calocluster.e2);
+        h_caloclusters_e9->Fill(calocluster.e9);
+        h_caloclusters_e25->Fill(calocluster.e25);
       }
     }
 
